8a04fb03ee7ca16bc929d38b19fd8f5ac45aff62
User8395 <qasim75426@gmail.com>
2024-01-18 14:51:53 -0500

    THE FIRST EVER COMMIT

---
+++ README.md
@@ -0,0 +1,39 @@
+# Serverman
+Web-based server frontend
+
+## What dis
+Serverman is a frontend for your server. It aims to manage all aspects of a server, including, but not limited to
+- Network connections
+- Packages
+- System updates
+- Installed server software
+
+## "Serverman"...sounds like a play on words
+It is! Serverman is short for **Server man**ager.
+
+## Does it work?
+As of now, Serverman is under heavy development. There is no feature timeline right now, so no one knows what features will come next.
+
+## I wanna try!
+If you want try out Serverman, just make sure you have Python 3 or newer installed and follow the steps below
+
+1. Clone the repo and `cd` into it: `git clone https://github.com/User8395/serverman; cd serverman`
+2. Install `python-venv`:
+	Debian/derivatives: `sudo apt install python-venv`
+	Fedora: `venv` comes with the base Python package
+	Arch/derivatives: `sudo pacman -S python-venv`
+	All others: refer to your distrubution's wiki
+3. Create a virtual enviroment: `python -m venv venv`
+4. Activate the virtual enviroment: `source venv/bin/activate`
+5. Install requirements: `pip install -r requirements.txt`
+6. Enter the source directory: `cd src`
+7. Run Serverman: `flask run --debug -p 6699`
+
+When you're done, deactivate the virtual enviroment by running `deactivate`.
+
+## What can Serverman do right now?
+Currently, Serverman can get system network information by using `ip a`. View it by going to Settings > Internet > Status.
+
+## Credits
+Pallets Projects: for Flask, Jinja, and Werkzeug
+Comedic README inspired by @dnschneid
---
+++ src/app.py
@@ -0,0 +1,54 @@
+from os import popen
+from flask import Flask, redirect
+from flask import render_template
+
+app = Flask(__name__, static_folder="static")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=6699)
+
+openapplets = []
+
+def checkifappletclosed(applet):
+    if applet not in openapplets:
+        return True
+    else:
+        return False
+
+@app.route("/")
+def login():
+    return redirect("/applets")
+
+@app.route("/quit/<applet>")
+def quit(applet):
+    openapplets.remove(applet)
+    return redirect("/applets")
+
+@app.route("/applets")
+def applets():
+    return render_template("applets.html", openapplets=openapplets)
+
+@app.route("/applets/<applet>")
+def applet(applet):
+    if checkifappletclosed(applet):
+        openapplets.append(applet)
+        print(openapplets)
+    return render_template(f"applets/{applet}/index.html", openapplets=openapplets)
+    
+@app.route("/applets/<applet>/<menu>/")
+def appletmenu(applet, menu):
+    return render_template(f"applets/{applet}/{menu}.html", openapplets=openapplets)
+
+@app.route("/applets/<applet>/<menu>/<menu1>")
+def appletmenu1(applet, menu, menu1):
+    if applet == "settings" and menu == "internet" and menu1 == "status":
+        return render_template(f"applets/settings/internet-status.html", openapplets=openapplets, ip=popen("ip a").read().replace("\n", "<br>"))
+    return render_template(f"applets/{applet}/{menu}-{menu1}.html", openapplets=openapplets)
+
+@app.route("/applets/<applet>/<menu>/<menu1>/<menu2>")
+def appletmenu2(applet, menu, menu1, menu2):
+    return render_template(f"applets/{applet}/{menu}-{menu1}-{menu2}.html", openapplets=openapplets)
+
+@app.route("/applets/<applet>/<menu>/<menu1>/<menu2>/<menu3>")
+def appletmenu3(applet, menu, menu1, menu2, menu3,):
+    return render_template(f"applets/{applet}/{menu}-{menu1}-{menu2}-{menu3}.html", openapplets=openapplets)
