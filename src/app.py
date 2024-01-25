9528ac204e3d68075729d4c7269be1a12f75166c
User8395 <qasim75426@gmail.com>
2024-01-24 21:22:43 -0500

    Add support for servermand

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 from json import load, dump
-from os import popen
-from flask import Flask, redirect
-from flask import render_template
+from os import path
+from time import sleep
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__, static_folder="static")
 
@@ -12,6 +12,11 @@ openapplets = []
 
 appletsvar = load(open("applets.json"))
 software = load(open("software.json"))
+settings = load(open("settings.json"))
+
+def savesettings():
+    with open("settings.json", "w") as f:
+        dump(settings, f)
 
 def checkifappletclosed(applet):
     if applet not in openapplets:
@@ -19,9 +24,35 @@ def checkifappletclosed(applet):
     else:
         return False
 
+def checkifservermandstarted():
+    if settings["daemon"]:
+        try:
+            with open(f"{settings['daemon']}servermand.pid") as f:
+                if path.isdir(f"/proc/{f.read()}"): 
+                    return True
+                else:
+                    return False
+        except FileNotFoundError:
+            return False
+    else:
+        return False
+
 @app.route("/")
 def login():
     return redirect("/applets")
+
+@app.route("/setdaemonpath", methods=['POST'])
+def setdaemonpath():
+    data = dict(request.form)
+    path = ""
+    if data["path"][-1] != "/":
+        path = data["path"] + "/"
+        print(path)
+    else:
+        path = data["path"]
+    settings["daemon"] = path
+    savesettings()
+    return redirect("/applets/settings/daemon")
 
 @app.route("/quit/<applet>")
 def quit(applet):
@@ -44,12 +75,25 @@ def applet(applet):
     
 @app.route("/applets/<applet>/<menu>/")
 def appletmenu(applet, menu):
+    if applet == "settings" and menu == "daemon":
+        return render_template("applets/settings/daemon.html", daemon=settings["daemon"])
     return render_template(f"applets/{applet}/{menu}.html", openapplets=openapplets)
 
 @app.route("/applets/<applet>/<menu>/<menu1>")
 def appletmenu1(applet, menu, menu1):
     if applet == "settings" and menu == "internet" and menu1 == "status":
-        return render_template(f"applets/settings/internet-status.html", openapplets=openapplets, ip=popen("ip a").read().replace("\n", "<br>"))
+        ip = ""
+        if checkifservermandstarted():
+            with open(f"{settings['daemon']}servermand.input", "w") as f:
+                f.write("internet status")
+                sleep(0.5)
+            with open(f"{settings['daemon']}servermand.output", "r") as f:
+                ip = f.read()
+        else:
+            return redirect("/applets/settings/daemon/notstarted")
+        return render_template(f"applets/settings/internet-status.html", openapplets=openapplets, ip=ip)
+    elif applet == "settings" and menu == "daemon" and menu1 == "setpath":
+        return render_template("applets/settings/daemon-setpath.html", daemon=settings["daemon"])
     return render_template(f"applets/{applet}/{menu}-{menu1}.html", openapplets=openapplets)
 
 @app.route("/applets/<applet>/<menu>/<menu1>/<menu2>")
@@ -57,5 +101,5 @@ def appletmenu2(applet, menu, menu1, menu2):
     return render_template(f"applets/{applet}/{menu}-{menu1}-{menu2}.html", openapplets=openapplets)
 
 @app.route("/applets/<applet>/<menu>/<menu1>/<menu2>/<menu3>")
-def appletmenu3(applet, menu, menu1, menu2, menu3,):
+def appletmenu3(applet, menu, menu1, menu2, menu3):
     return render_template(f"applets/{applet}/{menu}-{menu1}-{menu2}-{menu3}.html", openapplets=openapplets)
