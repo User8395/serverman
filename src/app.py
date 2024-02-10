158039c4585fcbc47a7932c40f6c94dd8b5810d0
Qasim Khan <qasim75426@gmail.com>
2024-02-10 18:12:48 -0500

    Redesign settings, add contactdaemon function for applets

--- src/app.py
+++ src/app.py
@@ -2,8 +2,10 @@ from json import load, dump
 from os import path
 from time import sleep
 from flask import Flask, redirect, render_template, request
+from ast import literal_eval
 
 app = Flask(__name__, static_folder="static")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=6699)
@@ -26,7 +28,7 @@ def checkifappletclosed(applet):
 
 
 def checkifservermandstarted():
-    if settings["daemon"]:
+    if settings["daemon"] == "":
         try:
             with open(f"{settings['daemon']}servermand.pid") as f:
                 if path.isdir(f"/proc/{f.read()}"): 
@@ -36,27 +38,52 @@ def checkifservermandstarted():
         except FileNotFoundError:
             return False
     else:
-        return False
+        return "nopath"
+    
+def contactdaemon(command):
+    if command == "daemonpath":
+        return settings["daemon"]
+    if checkifservermandstarted():
+        try:
+            with open(f"{settings['daemon']}servermand.input", "w") as f:
+                f.write(command)
+            with open(f"{settings['daemon']}servermand.output", "r") as f:
+                sleep(1)
+                return f.read()
+        except FileNotFoundError:
+            print("dERROR")
+            return "notstarted"
+    elif "nopath":
+        return "nopath"
+    else:
+        return "notstarted"
+
+@app.context_processor
+def globalvars():
+    return dict(openapplets=openapplets, literal_eval=literal_eval, contactdaemon=contactdaemon)
 
 @app.route("/")
 def login():
-    return redirect("/applets")
+    return redirect("/applets/")
 
-@app.route("/setdaemonpath", methods=['POST'])
+@app.route("/setdaemonpath/", methods=['POST'])
 def setdaemonpath():
     data = dict(request.form)
     daemonpath = ""
-    if data["path"][-1] != "/":
-        daemonpath = data["path"] + "/"
-    else:
-        daemonpath = data["path"]
+    try:
+        if data["path"][-1] != "/":
+            daemonpath = data["path"] + "/"
+        else:
+            daemonpath = data["path"]
+    except IndexError:
+        pass
     if path.exists(daemonpath + "servermand.py") == False:
         return redirect("/applets/settings/daemon/invalidpath/")
     settings["daemon"] = daemonpath
     savesettings()
     return redirect("/applets/settings/daemon")
 
-@app.route("/quit/<applet>")
+@app.route("/quit/<applet>/")
 def quit(applet):
     try:
         openapplets.remove(applet)
@@ -64,7 +91,7 @@ def quit(applet):
     except ValueError:
         return redirect("/applets")
 
-@app.route("/applets")
+@app.route("/applets/")
 def applets():
     appletsdict = {}
     for i, val in enumerate(appletsvar):
@@ -79,37 +106,10 @@ def applet(applet):
     
 @app.route("/applets/<applet>/<menu>/")
 def appletmenu(applet, menu):
-    if applet == "settings" and menu == "daemon":
-        return render_template("applets/settings/daemon.html", daemon=settings["daemon"])
     return render_template(f"applets/{applet}/{menu}.html", openapplets=openapplets)
 
 @app.route("/applets/<applet>/<menu>/<menu1>/")
 def appletmenu1(applet, menu, menu1):
-    if applet == "settings" and menu == "internet" and menu1 == "status":
-        ips = ""
-        if checkifservermandstarted():
-            with open(f"{settings['daemon']}servermand.input", "w") as f:
-                f.write("internet status")
-            with open(f"{settings['daemon']}servermand.output", "r") as f:
-                sleep(1)
-                ips = f.read()
-                print(ips)
-        else:
-            return redirect("/applets/settings/daemon/notstarted")
-        return render_template(f"applets/settings/internet-status.html", openapplets=openapplets, ip=ips)
-    elif applet == "settings" and menu == "internet" and menu1 == "changeaddress":
-        ips = []
-        if checkifservermandstarted():
-            with open(f"{settings['daemon']}servermand.input", "w") as f:
-                f.write("internet addresses")
-            with open(f"{settings['daemon']}servermand.output", "r") as f:
-                sleep(1)
-                ips = f.read().split()
-        else:
-            return redirect("/applets/settings/daemon/notstarted")
-        return render_template("applets/settings/internet-changeaddress.html", openapplets=openapplets, ips=enumerate(ips))
-    elif applet == "settings" and menu == "daemon" and menu1 == "setpath":
-        return render_template("applets/settings/daemon-setpath.html", daemon=settings["daemon"])
     return render_template(f"applets/{applet}/{menu}-{menu1}.html", openapplets=openapplets)
 
 @app.route("/applets/<applet>/<menu>/<menu1>/<menu2>/")
